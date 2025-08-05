f29af3e233db6b0bfcd11a4cbcf0761d95733d56
akshayborade09 <akshay.borade09@gmail.com>
2025-08-05 11:07:27 +0530

    changes

--- apps/www/config/site.ts
+++ apps/www/config/site.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
   name: "Krutrim Cloud UI",
-  url: "https://ui.shadcn.com",
+  url: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "https://ui.shadcn.com",
   description:
     "A set of beautifully-designed, accessible components and a code distribution platform. Works with your favorite frameworks. Open Source. Open Code.",
   links: {
     twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn-ui/ui",
+    github: "https://github.com/akshayborade09/krutrim-cloud",
   },
 }
 
